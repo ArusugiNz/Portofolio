6199176f16aa4cf63262d90443f270e13c93b807
ArusugiNz <naufalzaidn@gmail.com>
2025-08-04 12:41:25 +0700

    Delete Portofolio directory

--- Portofolio/java.js
+++
@@ -1,7 +0,0 @@
- const music = document.getElementById("bg-music");
-  const playLink = document.querySelector(".play-music");
-
-  playLink.addEventListener("click", function(e) {
-    e.preventDefault(); // Mencegah reload
-    music.play();
-  });
